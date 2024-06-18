2808b0283a66542ca70438faa7ea7ab37d3c5c87
Aidyn A. <reiaogiri@gmail.com>
2024-06-18 16:03:50 +0500

    removed duplicates

--- src/main/java/axgiri/github/REST_API_library/entity/ERole.java
+++
@@ -1,6 +0,0 @@
-// package axgiri.github.REST_API_library.entity;
-
-// public enum Role {
-//     USER_ROLE,
-//     AMIN_ROLE
-// }
--- src/main/java/axgiri/github/REST_API_library/repository/BookRepo.java
+++
@@ -1,9 +0,0 @@
-package axgiri.github.REST_API_library.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import axgiri.github.REST_API_library.entity.Book;
-
-public interface BookRepo extends JpaRepository<Book, Integer>{
-    
-}
